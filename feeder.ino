bd867e4868288c3bd02ac38aaa9ed939b310d850
Sawarin Sodsangsook <calopc.canova@gmail.com>
2018-04-07 16:05:13 +0700

    feeder

---
+++ feeder.ino
@@ -0,0 +1,99 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+#include  "virtuabotixRTC.h"
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+Servo myservo;  
+#define ONE_WIRE_BUS 2 
+#define SERVO_BUS 9
+int pos = 0;    // variable to store the servo position
+LiquidCrystal_I2C lcd(0x3f,20,4);
+OneWire oneWire(ONE_WIRE_BUS); 
+DallasTemperature sensors(&oneWire); 
+virtuabotixRTC myRTC(6, 7, 8);
+void setup()
+{
+//  myRTC.setDS1302Time(0, 40,20,0,0,0,0);     
+  myservo.attach(SERVO_BUS);  // attaches the servo on pin 9 to the servo object
+  myRTC.updateTime();
+  Serial.begin(9600);
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("TankTemp:");
+  lcd.setCursor(14,0);
+  lcd.print((char)223);
+  lcd.setCursor(15,0);
+  lcd.print("c");
+  lcd.setCursor(0,1);
+  lcd.print("Last fed:");
+  lcd.setCursor(9,1);
+  lcd.print("--");
+  lcd.setCursor(11,1);
+  lcd.print(":");
+  lcd.setCursor(12,1);
+  lcd.print("--");
+  
+  // Start up the library 
+  sensors.begin(); 
+}
+boolean needfeed(){
+  myRTC.updateTime();
+//  Serial.println(myRTC.hours%12);
+//  Serial.println(myRTC.minutes);
+//  Serial.println(myRTC.seconds);
+  if((myRTC.hours%10==0)&&myRTC.minutes==0&&myRTC.seconds==0){
+    return true;
+  }
+  return false;
+}
+
+
+void feed(){
+  lcd.setCursor(9,1);
+  int hour = myRTC.hours;
+  if (hour < 10) lcd.print("0"+String(myRTC.hours));
+  else lcd.print(String(myRTC.hours));
+  lcd.setCursor(12,1);
+  int minutes = myRTC.minutes;
+  if (minutes < 10) lcd.print("0"+String(myRTC.minutes));
+  else lcd.print(myRTC.minutes);
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(5);                       // waits 15ms for the servo to reach the position
+  }
+}
+void showTemp(){
+  sensors.requestTemperatures();
+  lcd.setCursor(9,0);
+  lcd.print(sensors.getTempCByIndex(0));
+  delay(1000); 
+}
+
+void loop()
+{
+  if(needfeed()){
+    feed();
+  }
+  showTemp();
+}
+
+
